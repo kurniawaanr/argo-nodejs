9589ddd9531bbc11d2bd91ef7cdd66cff7478bbf
kurniawaanr <pitesekur@yahoo.com>
2024-06-07 22:14:59 +0700

    fix dockerfile n jenkinsfile

--- dockerfile
+++ dockerfile
@@ -1,20 +1,5 @@
-# Use the official Node.js 14 image as the base image
-FROM node:14
-
-# Set the working directory to /app
+FROM node:current-alpine
+COPY . /app
 WORKDIR /app
-
-# Copy the package.json and package-lock.json files to the working directory
-COPY package*.json ./
-
-# Install the dependencies
 RUN npm install
-
-# Copy the rest of the files to the working directory
-COPY . .
-
-# Expose port 3000 for the app
-EXPOSE 3000
-
-# Run the app
-CMD ["node", "index.js"]
+ENTRYPOINT ["npm", "start"]
--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const http = require('node:http');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000; // Use the PORT environment variable provided by Azure Web App, or default to 3000
 
 const server = http.createServer((req, res) => {
     const filePath = path.join(__dirname, 'index.html');
@@ -22,6 +21,6 @@ const server = http.createServer((req, res) => {
     });
   });
   
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  server.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at ${port}`);
   });
